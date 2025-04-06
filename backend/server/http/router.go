f278c77964bd844ca2e3c404073a10d3f2e50c3b
aktky <aktky4d@gmail.com>
2025-04-06 21:59:28 +0900

    fix

--- backend/server/http/router.go
+++ backend/server/http/router.go
@@ -46,6 +46,7 @@ func (router *Router) Build() chi.Router {
 	r.Use(router.middleware.SetSubdomain)
 
 	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte(`{"status": "ok"}`))
 	})
