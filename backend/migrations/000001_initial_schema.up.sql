198c5d0b9541cc1a332b9675a7feea8fa73425a6
aktky <aktky4d@gmail.com>
2025-04-11 15:59:11 +0900

    fix ddl

--- backend/migrations/000001_initial_schema.up.sql
+++ backend/migrations/000001_initial_schema.up.sql
@@ -11,19 +11,19 @@ $$ LANGUAGE plpgsql;
 
 -- user table
 CREATE TABLE "user" (
-  "id"            UUID          NOT NULL,
-  "email"         VARCHAR(255)  NOT NULL,
-  "first_name"    VARCHAR(255)  NOT NULL,
-  "last_name"     VARCHAR(255)  NOT NULL,
-  "hashed_secret" VARCHAR(255)  NOT NULL,
-  "google_id"     VARCHAR(255)  NOT NULL,
-  "created_at"    TIMESTAMPTZ   NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  "updated_at"    TIMESTAMPTZ   NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  "id"                 UUID          NOT NULL,
+  "email"              VARCHAR(255)  NOT NULL,
+  "first_name"         VARCHAR(255)  NOT NULL,
+  "last_name"          VARCHAR(255)  NOT NULL,
+  "refresh_token_hash" VARCHAR(255)  NOT NULL,
+  "google_id"          VARCHAR(255)  NOT NULL,
+  "created_at"         TIMESTAMPTZ   NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  "updated_at"         TIMESTAMPTZ   NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY ("id")
 );
 
 CREATE UNIQUE INDEX idx_user_email ON "user" ("email");
-CREATE UNIQUE INDEX idx_user_hashed_secret ON "user" ("hashed_secret");
+CREATE UNIQUE INDEX idx_user_refresh_token_hash ON "user" ("refresh_token_hash");
 
 CREATE TRIGGER update_user_updated_at
     BEFORE UPDATE ON "user"
@@ -96,7 +96,7 @@ CREATE TABLE "user_invitation" (
   PRIMARY KEY ("id")
 );
 
-CREATE UNIQUE INDEX idx_user_invitation_email ON "user_invitation" ("email");
+CREATE UNIQUE INDEX idx_user_invitation_organization_email ON "user_invitation" ("organization_id", "email");
 
 CREATE TRIGGER update_user_invitation_updated_at
     BEFORE UPDATE ON "user_invitation"
