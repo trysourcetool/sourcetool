27e86026199934dc4121b3b09f12d6ebd2271e2a
aktky <aktky4d@gmail.com>
2025-04-20 22:16:59 +0900

    remove comment

--- backend/internal/app/port/dependencies.go
+++ backend/internal/app/port/dependencies.go
@@ -7,7 +7,6 @@ type Dependencies struct {
 	WSManager  WSManager
 }
 
-// NewDependency returns the old Dependency struct (deprecated, use NewDependencies).
 func NewDependencies(repo Repository, mailer Mailer, pubsub PubSub, wsManager WSManager) *Dependencies {
 	return &Dependencies{
 		Repository: repo,
