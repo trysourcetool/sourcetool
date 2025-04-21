19e99e3c4dc558035961697b7ab81b0f24131236
aktky <aktky4d@gmail.com>
2025-04-22 06:16:45 +0900

    fix

--- examples/go/user-management/main.go
+++ examples/go/user-management/main.go
@@ -88,7 +88,7 @@ func createUserPage(ui sourcetool.UIBuilder) error {
 
 func main() {
 	s := sourcetool.New(&sourcetool.Config{
-		APIKey:   "development_MEst0aYqiDmlAgS1laq5tXFA94ERtB8rMEst0aYqiDmlAgS1laq",
+		APIKey:   "your_api_key",
 		Endpoint: "ws://localhost:3000",
 	})
 
