12834deffe43d1c069f50763549c0f2c663e3ea1
aktky <aktky4d@gmail.com>
2025-04-22 05:48:31 +0900

    add listPagesBase function

--- backend/internal/server/page.go
+++ backend/internal/server/page.go
@@ -1,53 +1,32 @@
-//go:build !ee
-// +build !ee
-
 package server
 
 import (
-	"errors"
-	"net/http"
+	"context"
 
-	"github.com/gofrs/uuid/v5"
-
-	"github.com/trysourcetool/sourcetool/backend/internal"
 	"github.com/trysourcetool/sourcetool/backend/internal/core"
 	"github.com/trysourcetool/sourcetool/backend/internal/database"
-	"github.com/trysourcetool/sourcetool/backend/internal/errdefs"
 	"github.com/trysourcetool/sourcetool/backend/internal/server/responses"
 )
 
-func (s *Server) listPages(w http.ResponseWriter, r *http.Request) error {
-	ctx := r.Context()
-	envIDReq := r.URL.Query().Get("environmentId")
-	if envIDReq == "" {
-		return errdefs.ErrInvalidArgument(errors.New("environmentId is required"))
-	}
-
-	o := internal.CurrentOrganization(ctx)
-
-	envID, err := uuid.FromString(envIDReq)
-	if err != nil {
-		return errdefs.ErrInvalidArgument(err)
-	}
-
-	env, err := s.db.Environment().Get(ctx, database.EnvironmentByID(envID))
-	if err != nil {
-		return err
-	}
-
+func (s *Server) listPagesBase(ctx context.Context, env *core.Environment, o *core.Organization) (
+	[]*responses.PageResponse,
+	[]*responses.UserResponse,
+	[]*responses.UserGroupResponse,
+	error,
+) {
 	pages, err := s.db.Page().List(ctx, database.PageByOrganizationID(o.ID), database.PageByEnvironmentID(env.ID), database.PageOrderBy(`array_length(p."path", 1), "path"`))
 	if err != nil {
-		return err
+		return nil, nil, nil, err
 	}
 
 	users, err := s.db.User().List(ctx, database.UserByOrganizationID(o.ID))
 	if err != nil {
-		return err
+		return nil, nil, nil, err
 	}
 
 	userGroups, err := s.db.User().ListGroups(ctx, database.UserGroupByOrganizationID(o.ID))
 	if err != nil {
-		return err
+		return nil, nil, nil, err
 	}
 
 	pagesOut := make([]*responses.PageResponse, 0, len(pages))
@@ -65,11 +44,5 @@ func (s *Server) listPages(w http.ResponseWriter, r *http.Request) error {
 		userGroupsOut = append(userGroupsOut, responses.UserGroupFromModel(userGroup))
 	}
 
-	return s.renderJSON(w, http.StatusOK, responses.ListPagesResponse{
-		Pages:      pagesOut,
-		Groups:     make([]*responses.GroupResponse, 0),
-		GroupPages: make([]*responses.GroupPageResponse, 0),
-		Users:      usersOut,
-		UserGroups: userGroupsOut,
-	})
+	return pagesOut, usersOut, userGroupsOut, nil
 }
--- backend/internal/server/page_ce.go
+++ backend/internal/server/page_ce.go
@@ -1,5 +1,5 @@
-//go:build ee
-// +build ee
+//go:build !ee
+// +build !ee
 
 package server
 
@@ -39,30 +39,10 @@ func (s *Server) listPages(w http.ResponseWriter, r *http.Request) error {
 		return err
 	}
 
-	groups, err := s.db.Group().List(ctx, database.GroupByOrganizationID(o.ID))
-	if err != nil {
-		return err
-	}
-
-	groupPages, err := s.db.Group().ListPages(ctx, database.GroupPageByOrganizationID(o.ID), database.GroupPageByEnvironmentID(env.ID))
-	if err != nil {
-		return err
-	}
-
-	groupsOut := make([]*responses.GroupResponse, 0, len(groups))
-	for _, group := range groups {
-		groupsOut = append(groupsOut, responses.GroupFromModel(group))
-	}
-
-	groupPagesOut := make([]*responses.GroupPageResponse, 0, len(groupPages))
-	for _, groupPage := range groupPages {
-		groupPagesOut = append(groupPagesOut, responses.GroupPageFromModel(groupPage))
-	}
-
 	return s.renderJSON(w, http.StatusOK, responses.ListPagesResponse{
 		Pages:      pagesOut,
-		Groups:     groupsOut,
-		GroupPages: groupPagesOut,
+		Groups:     make([]*responses.GroupResponse, 0),
+		GroupPages: make([]*responses.GroupPageResponse, 0),
 		Users:      usersOut,
 		UserGroups: userGroupsOut,
 	})
