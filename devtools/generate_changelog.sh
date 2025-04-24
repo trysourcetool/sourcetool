4cb36a38548a4d8e77b188494c36c4c2e8158af1
aktky <aktky4d@gmail.com>
2025-04-24 19:10:01 +0900

    fix

--- devtools/generate_changelog.sh
+++ devtools/generate_changelog.sh
@@ -2,8 +2,9 @@
 set -e
 
 # This script generates a changelog from git history
-# Usage: ./generate_changelog.sh [previous_tag]
+# Usage: ./generate_changelog.sh [previous_tag] [path]
 # If previous_tag is not provided, it will be automatically detected
+# If path is provided, only commits affecting that path will be included
 
 # Get the previous tag if not provided
 if [ -z "$1" ]; then
@@ -12,14 +13,25 @@ else
   PREV_TAG=$1
 fi
 
+# Get the path filter if provided
+PATH_FILTER=$2
+
 # Generate changelog content
 {
   if [ -n "$PREV_TAG" ]; then
     echo "## Changes since $PREV_TAG"
-    git log --pretty=format:"* %s" $PREV_TAG..HEAD
+    if [ -n "$PATH_FILTER" ]; then
+      git log --pretty=format:"* %s" $PREV_TAG..HEAD -- "$PATH_FILTER"
+    else
+      git log --pretty=format:"* %s" $PREV_TAG..HEAD
+    fi
   else
     echo "## Initial Release"
-    git log --pretty=format:"* %s"
+    if [ -n "$PATH_FILTER" ]; then
+      git log --pretty=format:"* %s" -- "$PATH_FILTER"
+    else
+      git log --pretty=format:"* %s"
+    fi
   fi
   echo
 } 
